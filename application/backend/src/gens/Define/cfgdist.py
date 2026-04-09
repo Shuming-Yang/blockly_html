e3b8fea73168fbfee74aed689284b851be3cb7be
steven.yang <steven.yang@ovt.com>
2026-04-09 12:16:18 +0800

    Remove old files

--- application/backend/src/gens/Define/cfgdist.py
+++
@@ -1,8 +0,0 @@
-"""
-This GENS mode code which be ported from GENS source code.
-
-Author: OVT AE
-Date: 2024-11-01
-"""
-input_dict = {"SENSOR": ("SCCBID", "SCCBSPD", "SCCBNO"),
-              "SERDES": ("EN", "MODE", "SERNUM")}
